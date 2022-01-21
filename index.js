ba4af4fc47d8ca41e9ff05a950a25f128ae06774
pentester2301 <56609409+pentester2301@users.noreply.github.com>
2022-01-21 22:40:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+    const name = (req.query.name || (req.body && req.body.name));
+    const responseMessage = name
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: responseMessage
+    };
+}
